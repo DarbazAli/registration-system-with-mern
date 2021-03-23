9c21c80630cab81ef6168406bc8dd863e9fa985c
Darbaz Ali <darbaz.me@gmail.com>
2021-03-23 15:16:32 +0300

    DB Connection

--- backend/express.js
+++ backend/express.js
@@ -1,8 +1,16 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+import compress from 'compression'
+import cors from 'cors'
+import helmet from 'helmet'
 
 const app = express()
 
-app.use(express.json())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(compress())
+app.use(cors())
+app.use(helmet())
 
 app.get('/', (req, res) => {
   res.send('API is working')
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,27 @@
+'use strict'
+console.clear()
+
 import mongoose from 'mongoose'
 import app from './express.js'
 import config from '../config/config.js'
 
-// connect to db
+/* ------------------ CONNECT TO DB --------------------- */
+mongoose.Promise = global.Promise
+mongoose
+  .connect(config.mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('MongoDB connected successfully')
+  })
+
+mongoose.connection.on('error', () => {
+  throw new Error(`Unable to connect to database: ${config.mongoUri}`)
+})
+/* ---------------- END OF CONNECTION ----------------- */
 
 app.listen(config.port, () => {
   console.log(`Server running on port ${config.port}`)
