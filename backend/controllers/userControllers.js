f395507c295160fba9d7fafe91cec85244c0b0f8
Darbaz Ali <darbaz.me@gmail.com>
2021-03-23 15:51:41 +0300

    List all users

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,19 +1,40 @@
+import asyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
 import { errorHandler } from '../lib/errorMiddleware.js'
-// create new user
-const create = async (req, res) => {
+
+/*================================================================
+CREATE NEW SUER
+@desc   Create new user
+@route  POST /api/users
+@access Public
+================================================================*/
+const create = asyncHandler(async (req, res) => {
   const user = new User(req.body)
 
-  try {
-    await user.save()
+  const savedUser = await user.save()
+  if (savedUser) {
     return res.status(201).json({
       message: 'Successfully signed up!',
     })
-  } catch (error) {
-    return res.status(400).json({
-      error: errorHandler(error),
-    })
+  } else {
+    res.status(401)
+    throw new Error('Invalid user data')
   }
-}
+})
 
-export default { create }
+/*================================================================
+LIST ALL USERS
+@desc   List all users
+@route  GET /api/users
+@access Public
+================================================================*/
+const list = asyncHandler(async (req, res) => {
+  const users = await User.find({})
+  if (users) return res.status(200).json(users)
+  else {
+    res.status(401)
+    throw new Error('No users found')
+  }
+})
+
+export default { create, list }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,7 +2,7 @@ import express from 'express'
 import userCtrl from '../controllers/userControllers.js'
 const router = express.Router()
 
-router.route('/').post(userCtrl.create)
+router.route('/').post(userCtrl.create).get(userCtrl.list)
 
 // router
 //   .route('/:id')
